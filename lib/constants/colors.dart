a27f862688e1e7794c2c94bece327637228253fa
XCreateev <gpflutter@gmail.com>
2023-12-12 12:26:45 +0500

    pubspace yaml and some changes 2

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,4 +1,5 @@
-import 'dart:ui';
+
+import 'package:flutter/material.dart';
 
 const scaffoldBackgroundColor = _dark;
 const white = _white;
@@ -11,3 +12,55 @@ const _dark = Color(0XFF131524);
 const _white = Color(0xFFFFFFFF);
 const _whitish = Color(0xFFFEFEFE);
 const _blue = Color(0XFF4B7FD6);
+
+const black = _black;
+const navigationBarBackgroundColor = _dark;
+const activeColor = _accentBlue;
+const inactiveColor = _whitish;
+const appBarColor = _darkish;
+const onPrimaryColor = _whitish;
+const secondaryColor = _tuna;
+const errorColor = _pinkishRed;
+const textFieldBackgroundColor = _c2B304A;
+const cursorColor = _c4B7FD6;
+const textFieldBackgroundColor2 = _c1C233A;
+const textFieldBorderColor = _cfromRGBO;
+const hintTextColor = _whitish;
+const textButtonColor = _c4B7FD6;
+const wButtonColor = _c4B7FD6;
+var disabledButtonColor = _c4B7FD6.withOpacity(.3);
+const socialMediaButtonColor = _c272C38;
+const pinPutBorderColor = _c006EE9;
+const green = Colors.green;
+const verificationPageText = _c979797;
+const verificationPageText2 = _c407BFF;
+const pink = _cDD8491;
+//
+const orange = _cFF8700;
+const grey = _grey;
+const meetCam = _meetCamColor;
+const conNot = _container;
+const purpleIconColor = _c525298;
+//
+
+const _black = Color(0xFF000000);
+const _accentBlue = Color(0xFF4B7FD6);
+const _darkish = Color(0xFF131524);
+const _tuna = Color(0xFF2B304A);
+const _pinkishRed = Color(0xFFFF2752);
+const _c2B304A = Color(0xFF2B304A);
+const _c4B7FD6 = Color(0xFF4B7FD6);
+const _c1C233A = Color(0xFF1C233A);
+const _cfromRGBO = Color.fromRGBO(0, 110, 233, 0.10);
+const _c272C38 = Color(0xFF272C38);
+const _c006EE9 = Color(0xFF006EE9);
+const _c979797 = Color(0xFF979797);
+const _c407BFF = Color(0xFF407BFF);
+
+//
+const _cFF8700 = Color(0xFFFF8700);
+const _grey = Color(0xFFABABAB);
+const _meetCamColor = Color(0xFF22B07D);
+const _container = Color(0xFF323440);
+const _cDD8491 = Color(0xFFDD8491);
+const _c525298 = Color(0xFF525298);
---
+++ lib/constants/images.dart
@@ -0,0 +1,13 @@
+const _images = 'assets/images/';
+
+sealed class AppImages {
+  static const profilePicture = '${_images}profile_picture.png';
+  static const onboarding1 = '${_images}onboarding.png';
+  static const onboarding2 = '${_images}onboarding1.png';
+  static const createBook = '${_images}create_book.png';
+  static const passwordBook = '${_images}password_book.png';
+  static const memoriseBook = '${_images}memorise_book.png';
+  static const plus = '${_images}plus.png';
+  static const boxPicture = '${_images}box_picture.png';
+  static const voice = '${_images}voice.png';
+}
